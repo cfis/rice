313b80a45f39ebde4a561d23d67fe59fbd0f04ac
Charlie Savage <cfis@savagexi.com>
2021-01-14 22:51:39 -0800

    Update the unit test framework to support comparing two Ruby values.

--- test/unittest.hpp
+++ test/unittest.hpp
@@ -15,6 +15,8 @@
 #include <sstream>
 #include <iostream>
 
+#include "ruby.h"
+
 #if !defined(_MSC_VER)
 using namespace std;
 #endif
@@ -208,6 +210,11 @@ inline bool is_equal(char * lhs, char const * rhs)
 inline bool is_equal(char const * lhs, char * rhs)
 {
   return std::string(lhs) == std::string(rhs);
+}
+
+inline bool is_equal(VALUE lhs, VALUE rhs)
+{
+  return rb_equal(lhs, rhs);
 }
 
 template<typename T, typename U>
